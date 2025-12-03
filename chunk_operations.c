a14828d42460dd6f186120b0fb2ebfa3b200c01e
z1qd <controlurselfziad@gmail.com>
2025-12-03 06:07:58 +0200

    Add files via upload

---
+++ chunk_operations.c
@@ -0,0 +1,199 @@
+/*
+ * chunk_operations.c
+ *
+ *  Created on: Oct 12, 2022
+ *      Author: HP
+ */
+
+#include <kern/trap/fault_handler.h>
+#include <kern/disk/pagefile_manager.h>
+#include <kern/proc/user_environment.h>
+#include "kheap.h"
+#include "memory_manager.h"
+#include <inc/queue.h>
+
+//extern void inctst();
+
+/******************************/
+/*[1] RAM CHUNKS MANIPULATION */
+/******************************/
+
+//===============================
+// 1) CUT-PASTE PAGES IN RAM:
+//===============================
+//This function should cut-paste the given number of pages from source_va to dest_va on the given page_directory
+//	If the page table at any destination page in the range is not exist, it should create it
+//	If ANY of the destination pages exists, deny the entire process and return -1. Otherwise, cut-paste the number of pages and return 0
+//	ALL 12 permission bits of the destination should be TYPICAL to those of the source
+//	The given addresses may be not aligned on 4 KB
+int cut_paste_pages(uint32* page_directory, uint32 source_va, uint32 dest_va, uint32 num_of_pages)
+{
+	//TODO: PRACTICE: fill this function.
+	//Comment the following line
+	panic("cut_paste_pages() is not implemented yet...!!");
+}
+
+//===============================
+// 2) COPY-PASTE RANGE IN RAM:
+//===============================
+//This function should copy-paste the given size from source_va to dest_va on the given page_directory
+//	Ranges DO NOT overlapped.
+//	If ANY of the destination pages exists with READ ONLY permission, deny the entire process and return -1.
+//	If the page table at any destination page in the range is not exist, it should create it
+//	If ANY of the destination pages doesn't exist, create it with the following permissions then copy.
+//	Otherwise, just copy!
+//		1. WRITABLE permission
+//		2. USER/SUPERVISOR permission must be SAME as the one of the source
+//	The given range(s) may be not aligned on 4 KB
+int copy_paste_chunk(uint32* page_directory, uint32 source_va, uint32 dest_va, uint32 size)
+{
+	//TODO: PRACTICE: fill this function.
+	//Comment the following line
+	panic("copy_paste_chunk() is not implemented yet...!!");
+}
+
+//===============================
+// 3) SHARE RANGE IN RAM:
+//===============================
+//This function should share the given size from source_va to dest_va on the given page_directory
+//	Ranges DO NOT overlapped.
+//	It should set the permissions of the second range by the given perms
+//	If ANY of the destination pages exists, deny the entire process and return -1.
+//	Otherwise, share the required range and return 0
+//	During the share process:
+//		1. If the page table at any destination page in the range is not exist, it should create it
+//	The given range(s) may be not aligned on 4 KB
+int share_chunk(uint32* page_directory, uint32 source_va,uint32 dest_va, uint32 size, uint32 perms)
+{
+	//TODO: PRACTICE: fill this function.
+	//Comment the following line
+	panic("share_chunk() is not implemented yet...!!");
+}
+
+//===============================
+// 4) ALLOCATE CHUNK IN RAM:
+//===============================
+//This function should allocate the given virtual range [<va>, <va> + <size>) in the given address space  <page_directory> with the given permissions <perms>.
+//	If ANY of the destination pages exists, deny the entire process and return -1. Otherwise, allocate the required range and return 0
+//	If the page table at any destination page in the range is not exist, it should create it
+//	Allocation should be aligned on page boundary. However, the given range may be not aligned.
+int allocate_chunk(uint32* page_directory, uint32 va, uint32 size, uint32 perms)
+{
+	//TODO: PRACTICE: fill this function.
+	//Comment the following line
+	panic("allocate_chunk() is not implemented yet...!!");
+}
+
+
+//=====================================
+// 5) CALCULATE FREE SPACE:
+//=====================================
+//It should count the number of free pages in the given range [va1, va2)
+//(i.e. number of pages that are not mapped).
+//Addresses may not be aligned on page boundaries
+uint32 calculate_free_space(uint32* page_directory, uint32 sva, uint32 eva)
+{
+	//TODO: PRACTICE: fill this function.
+	//Comment the following line
+	panic("calculate_free_space() is not implemented yet...!!");
+}
+
+//=====================================
+// 6) CALCULATE ALLOCATED SPACE:
+//=====================================
+void calculate_allocated_space(uint32* page_directory, uint32 sva, uint32 eva, uint32 *num_tables, uint32 *num_pages)
+{
+	//TODO: PRACTICE: fill this function.
+	//Comment the following line
+	panic("calculate_allocated_space() is not implemented yet...!!");
+}
+
+//=====================================
+// 7) CALCULATE REQUIRED FRAMES IN RAM:
+//=====================================
+//This function should calculate the required number of pages for allocating and mapping the given range [start va, start va + size) (either for the pages themselves or for the page tables required for mapping)
+//	Pages and/or page tables that are already exist in the range SHOULD NOT be counted.
+//	The given range(s) may be not aligned on 4 KB
+uint32 calculate_required_frames(uint32* page_directory, uint32 sva, uint32 size)
+{
+	//TODO: PRACTICE: fill this function.
+	//Comment the following line
+	panic("calculate_required_frames() is not implemented yet...!!");
+}
+
+//=================================================================================//
+//===========================END RAM CHUNKS MANIPULATION ==========================//
+//=================================================================================//
+
+/*******************************/
+/*[2] USER CHUNKS MANIPULATION */
+/*******************************/
+
+//======================================================
+/// functions used for USER HEAP (malloc, free, ...)
+//======================================================
+
+//=====================================
+/* DYNAMIC ALLOCATOR SYSTEM CALLS */
+//=====================================
+/*2024*/
+void* sys_sbrk(int numOfPages)
+{
+	panic("not implemented function");
+}
+
+//=====================================
+// 1) ALLOCATE USER MEMORY:
+//=====================================
+void allocate_user_mem(struct Env* e, uint32 virtual_address, uint32 size)
+{
+	/*====================================*/
+	/*Remove this line before start coding*/
+//		inctst();
+//		return;
+	/*====================================*/
+
+	//TODO: [PROJECT'25.IM#2] USER HEAP - #2 allocate_user_mem
+	//Your code is here
+	//Comment the following line
+	panic("allocate_user_mem() is not implemented yet...!!");
+}
+
+//=====================================
+// 2) FREE USER MEMORY:
+//=====================================
+void free_user_mem(struct Env* e, uint32 virtual_address, uint32 size)
+{
+	/*====================================*/
+	/*Remove this line before start coding*/
+//		inctst();
+//		return;
+	/*====================================*/
+
+	//TODO: [PROJECT'25.IM#2] USER HEAP - #4 free_user_mem
+	//Your code is here
+	//Comment the following line
+	panic("free_user_mem() is not implemented yet...!!");
+}
+
+//=====================================
+// 4) FREE USER MEMORY (BUFFERING):
+//=====================================
+void __free_user_mem_with_buffering(struct Env* e, uint32 virtual_address, uint32 size)
+{
+	// your code is here, remove the panic and write your code
+	panic("__free_user_mem_with_buffering() is not implemented yet...!!");
+}
+
+//=====================================
+// 3) MOVE USER MEMORY:
+//=====================================
+void move_user_mem(struct Env* e, uint32 src_virtual_address, uint32 dst_virtual_address, uint32 size)
+{
+	panic("move_user_mem() is not implemented yet...!!");
+}
+
+//=================================================================================//
+//========================== END USER CHUNKS MANIPULATION =========================//
+//=================================================================================//
+
---
+++ uheap.c
@@ -0,0 +1,164 @@
+#include <inc/lib.h>
+
+//==================================================================================//
+//============================== GIVEN FUNCTIONS ===================================//
+//==================================================================================//
+
+//==============================================
+// [1] INITIALIZE USER HEAP:
+//==============================================
+int __firstTimeFlag = 1;
+void uheap_init()
+{
+	if(__firstTimeFlag)
+	{
+		initialize_dynamic_allocator(USER_HEAP_START, USER_HEAP_START + DYN_ALLOC_MAX_SIZE);
+		uheapPlaceStrategy = sys_get_uheap_strategy();
+		uheapPageAllocStart = dynAllocEnd + PAGE_SIZE;
+		uheapPageAllocBreak = uheapPageAllocStart;
+
+		__firstTimeFlag = 0;
+	}
+}
+
+//==============================================
+// [2] GET A PAGE FROM THE KERNEL FOR DA:
+//==============================================
+int get_page(void* va)
+{
+	int ret = __sys_allocate_page(ROUNDDOWN(va, PAGE_SIZE), PERM_USER|PERM_WRITEABLE|PERM_UHPAGE);
+	if (ret < 0)
+		panic("get_page() in user: failed to allocate page from the kernel");
+	return 0;
+}
+
+//==============================================
+// [3] RETURN A PAGE FROM THE DA TO KERNEL:
+//==============================================
+void return_page(void* va)
+{
+	int ret = __sys_unmap_frame(ROUNDDOWN((uint32)va, PAGE_SIZE));
+	if (ret < 0)
+		panic("return_page() in user: failed to return a page to the kernel");
+}
+
+//==================================================================================//
+//============================ REQUIRED FUNCTIONS ==================================//
+//==================================================================================//
+
+//=================================
+// [1] ALLOCATE SPACE IN USER HEAP:
+//=================================
+void* malloc(uint32 size)
+{
+	//==============================================================
+	//DON'T CHANGE THIS CODE========================================
+	uheap_init();
+	if (size == 0) return NULL ;
+	//==============================================================
+	//TODO: [PROJECT'25.IM#2] USER HEAP - #1 malloc
+	//Your code is here
+	//Comment the following line
+	panic("malloc() is not implemented yet...!!");
+}
+
+//=================================
+// [2] FREE SPACE FROM USER HEAP:
+//=================================
+void free(void* virtual_address)
+{
+	//TODO: [PROJECT'25.IM#2] USER HEAP - #3 free
+	//Your code is here
+	//Comment the following line
+	panic("free() is not implemented yet...!!");
+}
+
+//=================================
+// [3] ALLOCATE SHARED VARIABLE:
+//=================================
+void* smalloc(char *sharedVarName, uint32 size, uint8 isWritable)
+{
+	//==============================================================
+	//DON'T CHANGE THIS CODE========================================
+	uheap_init();
+	if (size == 0) return NULL ;
+	//==============================================================
+
+	//TODO: [PROJECT'25.IM#3] SHARED MEMORY - #2 smalloc
+	//Your code is here
+	//Comment the following line
+	panic("smalloc() is not implemented yet...!!");
+}
+
+//========================================
+// [4] SHARE ON ALLOCATED SHARED VARIABLE:
+//========================================
+void* sget(int32 ownerEnvID, char *sharedVarName)
+{
+	//==============================================================
+	//DON'T CHANGE THIS CODE========================================
+	uheap_init();
+	//==============================================================
+
+	//TODO: [PROJECT'25.IM#3] SHARED MEMORY - #4 sget
+	//Your code is here
+	//Comment the following line
+	panic("sget() is not implemented yet...!!");
+}
+
+
+//==================================================================================//
+//============================== BONUS FUNCTIONS ===================================//
+//==================================================================================//
+
+
+//=================================
+// REALLOC USER SPACE:
+//=================================
+//	Attempts to resize the allocated space at "virtual_address" to "new_size" bytes,
+//	possibly moving it in the heap.
+//	If successful, returns the new virtual_address, in which case the old virtual_address must no longer be accessed.
+//	On failure, returns a null pointer, and the old virtual_address remains valid.
+
+//	A call with virtual_address = null is equivalent to malloc().
+//	A call with new_size = zero is equivalent to free().
+
+//  Hint: you may need to use the sys_move_user_mem(...)
+//		which switches to the kernel mode, calls move_user_mem(...)
+//		in "kern/mem/chunk_operations.c", then switch back to the user mode here
+//	the move_user_mem() function is empty, make sure to implement it.
+void *realloc(void *virtual_address, uint32 new_size)
+{
+	//==============================================================
+	//DON'T CHANGE THIS CODE========================================
+	uheap_init();
+	//==============================================================
+	panic("realloc() is not implemented yet...!!");
+}
+
+
+//=================================
+// FREE SHARED VARIABLE:
+//=================================
+//	This function frees the shared variable at the given virtual_address
+//	To do this, we need to switch to the kernel, free the pages AND "EMPTY" PAGE TABLES
+//	from main memory then switch back to the user again.
+//
+//	use sys_delete_shared_object(...); which switches to the kernel mode,
+//	calls delete_shared_object(...) in "shared_memory_manager.c", then switch back to the user mode here
+//	the delete_shared_object() function is empty, make sure to implement it.
+void sfree(void* virtual_address)
+{
+	//TODO: [PROJECT'25.BONUS#5] EXIT #2 - sfree
+	//Your code is here
+	//Comment the following line
+	panic("sfree() is not implemented yet...!!");
+
+	//	1) you should find the ID of the shared variable at the given address
+	//	2) you need to call sys_freeSharedObject()
+}
+
+
+//==================================================================================//
+//========================== MODIFICATION FUNCTIONS ================================//
+//==================================================================================//
